608e6f1bdc795f716cbdbd9e90ecc6e92a7ca2d9
Duc Xuan Viet Nguyen <cookie@cuberates-465.local>
2026-02-20 13:24:30 -0800

    initialize springboot project

---
+++ src/main/java/com/example/cmpt276/upsolve/UpsolveIoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cmpt276.upsolve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpsolveIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpsolveIoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cmpt276/upsolve/UpsolveIoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cmpt276.upsolve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpsolveIoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
